a5935465f34274dc9cdd174329ab66297274c51e
sunseehray <sabtirazona@byui.edu>
2023-04-17 16:25:27 +0800

    javascript

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const d = new Date();
+const year = d.getFullYear();
+document.querySelector('#footer-year').textContent = year;
+
+const update = "April 17, 2023";
+document.querySelector('#date-of-update').textContent = update;

4e21b10be0d01a5302a5789cbee20b3c955f275f
sunseehray <sabtirazona@byui.edu>
2023-04-21 18:19:03 +0800

    simplified dates

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,2 @@
-// copyright year
-let d = new Date();
-let year = d.getFullYear();
-document.querySelector('#footerYear').innerHTML = year;
-
-// Last Update
-const options = {
-    weekday: "short",
-    month: "short",
-    day: "numeric",
-    year: "numeric"
-};
-document.querySelector('#dateLastUpdated').innerHTML = `Last Updated: ${new Date(document.lastModified).toLocaleDateString("en-US", options)}`;
+document.querySelector('#footerYear').innerHTML = new Date().getFullYear();
+document.querySelector('#dateLastUpdated').innerHTML = new Date(document.lastModified);

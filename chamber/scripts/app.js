47e34587b57a552ff1174278d01ddafec6336e48
sunseehray <sabtirazona@byui.edu>
2023-05-31 20:52:25 +0800

    numvisits

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -54,3 +54,19 @@ images.forEach(image => {
     imgObserver.observe(image);
 })
 
+// number of visits
+
+const visitsDisplay = document.querySelector("#num-visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits != 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🎉 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+

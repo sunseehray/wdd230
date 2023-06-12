a1eab2f5ff7be409163c2bcb77dd9d124ce432bf
sunseehray <sabtirazona@byui.edu>
2023-06-12 16:28:38 +0800

    asdasda

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -7,19 +7,27 @@ function displayProphets (prophets) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let portrait = document.createElement('img');
+
+      let dob = document.createElement('h3');
+      let pob = document.createElement('h3');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${prophet.name} ____________`;
-  
+      h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+      dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+      pob.textContent = `Place of Birth: ${prophet.birthplace}`;
+
       // Build the image portrait by setting all the relevant attribute
       portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portait of ${prophet.name} ______________`);
+      portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
       portrait.setAttribute('loading', 'lazy');
       portrait.setAttribute('width', '340');
       portrait.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(h2);
+      card.appendChild(dob);
+      card.appendChild(pob);
       card.appendChild(portrait);
   
       cards.appendChild(card);
@@ -33,7 +41,7 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets); 
+    console.table(data.prophets); 
     prophetList = data.prophets;
     displayProphets(prophetList);
   }

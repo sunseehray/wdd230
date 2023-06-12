9e8c274a8ed2a2f999af32fe4608adfabedf772f
sunseehray <sabtirazona@byui.edu>
2023-06-12 16:20:38 +0800

    finally json

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,31 +1,41 @@
+function displayProphets (prophets) {
+    
+    const cards = document.querySelector('div.cards'); // select the output container element
+  
+    prophets.forEach(prophet => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let portrait = document.createElement('img');
+  
+      // Build the h2 content out to show the prophet's full name - finish the template string
+      h2.textContent = `${prophet.name} ____________`;
+  
+      // Build the image portrait by setting all the relevant attribute
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portait of ${prophet.name} ______________`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(portrait);
+  
+      cards.appendChild(card);
+    }) // end of forEach loop
+  } // end of function expression
+
+prophetList = {};
+
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
-}
+    //console.table(data.prophets); 
+    prophetList = data.prophets;
+    displayProphets(prophetList);
+  }
 
 getProphetData();
-
-const displayProphets = (prophets) => {
-    const cards = document.querySelector('div.cards');
-
-    prophets.forEach((prophet) => {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let portrait = document.createElement('img');
-
-        h2.textContent = `${prophet.name} ___________`;
-
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ___________`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height','440');
-
-        card.appendChild(h2);
-        card.appendChild(portrait);
-
-        cards.appendChild(card);
-    })
-}

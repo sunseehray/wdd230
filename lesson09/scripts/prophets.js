e97360ddad2660049a2eb44ae9b4ad64d6b8606b
sunseehray <sabtirazona@byui.edu>
2023-06-12 15:19:33 +0800

    lesson 9

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,31 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards');
+
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        h2.textContent = `${prophet.name} ___________`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ___________`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height','440');
+
+        card.appendChild(h2);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    })
+}

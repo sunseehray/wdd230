54c0dd95b21bcf00d6c21b6c076eaac9354b2a28
sunseehray <sabtirazona@byui.edu>
2023-06-16 22:39:21 +0800

    haysawakas

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,15 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+})
+
+gridbutton.addEventListener("click", () => {
+    display.classList.remove("list");
+})
+
 // display in gallery mode
 
 function displayMembers (members) {
@@ -8,26 +20,35 @@ function displayMembers (members) {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let h3 = document.createElement('h3');
+      let logoContainer = document.createElement('picture');
+      let websiteImg = document.createElement('a');
       let website = document.createElement('a');
       let logo = document.createElement('img');
       let desc = document.createElement('p');
+      let contact = document.createElement('div');
+      contact.setAttribute('class','contact');
+      let address = document.createElement('p');
+      address.setAttribute('class','address');
       let phone = document.createElement('p');
-      let email = document.createElement('p');
-  
+
       // Build the card content
       h3.textContent = `${member.businessName}`;
       desc.textContent = `${member.businessDescription}`;
+      desc.setAttribute("class","desc");
 
-      phone.textContent = `📞${member.phone}`;
+      address.textContent = `${member.address}`;
+
+      phone.textContent = `${member.phone}`;
       phone.setAttribute('id','phone');
 
-      email.textContent = `📨${member.email}`;
-      email.setAttribute('id','email');
-
-
-      // Set logo link
+      website.textContent = `${member.website}`;
       website.setAttribute('href', member.website);
       website.setAttribute('target',"_blank");
+      website.setAttribute('id',"full-link");
+
+      // websiteImg for grid
+      websiteImg.setAttribute('href', member.website);
+      websiteImg.setAttribute('target',"_blank");
 
       // Build the image portrait by setting all the relevant attribute
       logo.setAttribute('src', member.imageURL);
@@ -40,10 +61,14 @@ function displayMembers (members) {
       // Append the section(card) with the created elements
       card.appendChild(h3);
       card.appendChild(desc);
-      card.appendChild(website);
-      website.appendChild(logo);
-      card.appendChild(phone);
-      card.appendChild(email);
+      card.appendChild(contact);
+      contact.appendChild(address);
+      contact.appendChild(phone);
+      contact.appendChild(website);
+      card.appendChild(logoContainer);
+      logoContainer.appendChild(websiteImg);
+      websiteImg.appendChild(logo);
+
   
       cards.appendChild(card);
     }) // end of forEach loop

4d54fe2656ceabd63e61e4d64eaa531259eedad3
sunseehray <sabtirazona@byui.edu>
2023-07-05 23:38:38 +0800

    fruit data fetch test

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -1,16 +1,18 @@
 const fruitData = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
 
-async function apiFetch(url) {
+async function fruitFetch(url) {
     try {
         const response = await fetch(url);
         if (response.ok) {
-            const data = await response.json;
-            console.log(data);
+            const data = await response.json();
+            console.table(data);
         } else {
             throw Error(await response.text());
         }  
     } catch (error) {
         console.log(error)
     }
-
 }
+
+
+fruitFetch(fruitData);

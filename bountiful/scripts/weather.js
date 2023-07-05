a52036e693d5859f6d138eb960a1e27c406a5a1e
sunseehray <sabtirazona@byui.edu>
2023-07-05 11:49:02 +0800

    started weather

--- bountiful/scripts/weather.js
+++ bountiful/scripts/weather.js
@@ -1,12 +1,13 @@
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=db9906a97dca16acb8595cd219ca129b';
+const currentweather = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=db9906a97dca16acb8595cd219ca129b';
 
-async function apiFetch() {
+const forecastweather = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&units=imperial&appid=db9906a97dca16acb8595cd219ca129b';
+
+async function apiFetch(url) {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            // console.log(data);
-            displayResults(data);
+            console.log(data);
         } else {
             throw Error(await response.text());
         }
@@ -15,4 +16,6 @@ async function apiFetch() {
     }
 }
 
-apiFetch();
+apiFetch(currentweather);
+
+apiFetch(forecastweather);

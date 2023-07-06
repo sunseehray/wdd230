39960c9bfd9a72c75852b178b5241f57efa93ac3
sunseehray <sabtirazona@byui.edu>
2023-07-06 19:45:56 +0800

    added options using fruit data

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -6,6 +6,7 @@ async function fruitFetch(url) {
         if (response.ok) {
             const data = await response.json();
             console.table(data);
+            createOption(data);
         } else {
             throw Error(await response.text());
         }  
@@ -14,5 +15,28 @@ async function fruitFetch(url) {
     }
 }
 
+
+function createOption(data) {
+    const option1 = document.querySelector('select#option1');
+    const option2 = document.querySelector('select#option2');
+    const option3 = document.querySelector('select#option3');
+
+    const fruits = data;
+
+    function addOption (element) {
+        fruits.forEach(fruit => {
+            let option = document.createElement('option');
+            option.setAttribute("value",fruit.name);
+            option.textContent = `${fruit.name}`;
+    
+            element.appendChild(option);
+            // console.log(fruit);
+        });            
+    }
+
+    addOption(option1);
+    addOption(option2);
+    addOption(option3);
+}
 
 fruitFetch(fruitData);
